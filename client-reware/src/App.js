767379926466f7c8779a9509bc740b6a562bad47
dev patel <dspatel0006@gmail.com>
2025-07-12 12:30:31 +0530

    setup to backend

--- client-reware/src/App.js
+++ client-reware/src/App.js
@@ -4,7 +4,6 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/login';
 import Signup from './pages/signup';
 import Dashboard from './pages/dashboard';
-import UserDashboard from './pages/UserDashboard';
 import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
@@ -13,18 +12,6 @@ function App() {
       <Route path="/" element={<Navigate to="/login" replace />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
-<<<<<<< Updated upstream
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route 
-        path="/userDashboard" 
-        element={
-            <UserDashboard />
-          // <ProtectedRoute>
-          // </ProtectedRoute>
-        } 
-      />
-      {/* Add more routes here */}
-=======
       <Route 
         path="/dashboard" 
         element={
@@ -34,7 +21,6 @@ function App() {
         } 
       />
       {/* Add more protected routes here */}
->>>>>>> Stashed changes
     </Routes>
   );
 }
